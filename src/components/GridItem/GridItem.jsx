45f12ea6b158fedf3a5874427b7cc300ffd548d7
Danulo <danilyanishevski2001@gmail.com>
2024-03-09 17:27:56 +0200

    finish

--- src/components/GridItem/GridItem.jsx
+++ src/components/GridItem/GridItem.jsx
@@ -1,3 +1,5 @@
-export const GridItem = () => {
-  return <h3>GridItem</h3>;
+import css from './GridItem.module.css';
+
+export const GridItem = ({ children }) => {
+  return <li className={css.item}>{children}</li>;
 };
--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,17 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { Grid, PhotosGalleryItem } from '..';
+
+export const PhotosGallery = ({ images, modalOpen }) => {
+  return (
+    <Grid>
+      {images.map(({ id, alt, avg_color, src }) => (
+        <PhotosGalleryItem
+          key={id}
+          alt={alt}
+          avg_color={avg_color}
+          src={src}
+          modalOpen={modalOpen}
+        />
+      ))}
+    </Grid>
+  );
 };
